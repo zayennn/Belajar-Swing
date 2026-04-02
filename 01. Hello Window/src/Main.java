ccad15ddab0a2898729dbdede26d7d427127cfc7
zayennn <zaayeennn@gmail.com>
2026-04-02 18:15:50 +0700

    update

--- 01. Hello Window/src/Main.java
+++ 01. Hello Window/src/Main.java
@@ -2,6 +2,10 @@ import javax.swing.JFrame;
 
 public class Main {
     public static void main(String[] args) {
+        JFrame frame = new JFrame("aplikasi pertama");
 
+        frame.setSize(400, 300);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
     }
 }
