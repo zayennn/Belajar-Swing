ca310f69d263d06b73b7629e0c7cd91eff7cf942
zayennn <zaayeennn@gmail.com>
2026-04-02 18:55:59 +0700

    update

--- 03. Text Field/src/Main.java
+++ 03. Text Field/src/Main.java
@@ -4,3 +4,23 @@ import javax.swing.JTextField;
 import javax.swing.JLabel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("text field");
+        frame.setLayout(null);
+
+        JLabel label = new JLabel("Masukan nama anda : ");
+        label.setBounds(20, 20, 120, 30);
+
+        JTextField textField = new JTextField();
+        textField.setBounds(20, 40, 140, 30);
+
+        JButton button = new JButton("submit");
+        button.setBounds(20, 60, 150, 40);
+
+        frame.setSize(400, 300);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
