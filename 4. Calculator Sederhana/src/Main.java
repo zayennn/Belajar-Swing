5a9dc83f93abae9baf2524b9a79755ac05972127
zayennn <zaayeennn@gmail.com>
2026-04-02 19:18:53 +0700

    update

--- 4. Calculator Sederhana/src/Main.java
+++ 4. Calculator Sederhana/src/Main.java
@@ -5,3 +5,14 @@ import javax.swing.JOptionPane;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("kalkulator sederhana");
+        frame.setLayout(null);
+
+        JLabel label1 = new JLabel();
+        label1.setBounds(20, 20, 150, 50);
+
+
+    }
+}
