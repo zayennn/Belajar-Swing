957ecbcdb68d8ac4036ed276ad1cd4d79af90079
zayennn <zaayeennn@gmail.com>
2026-04-02 19:30:57 +0700

    update

--- 4. Calculator Sederhana/src/Main.java
+++ 4. Calculator Sederhana/src/Main.java
@@ -13,7 +13,27 @@ public class Main {
         JLabel label1 = new JLabel("Masukan angka pertama : ");
         label1.setBounds(20, 20, 150, 50);
 
+        JTextField angka1 = new JTextField();
+        angka1.setBounds(20, 60, 150, 30);
+
+        JLabel label2 = new JLabel("Masukan angka kedua : ");
+        label2.setBounds(20, 80, 150, 50);
+
+        JTextField angka2 = new JTextField();
+        angka2.setBounds(20, 120, 150, 30);
+
+        JLabel label3 = new JLabel("Pilih operator (+, -, *, /) : ");
+        label3.setBounds(20, 140, 150, 50);
+
+        JTextField operator = new JTextField();
+        operator.setBounds(20, 160, 150, 30);
+
         frame.add(label1);
+        frame.add(angka1);
+        frame.add(label2);
+        frame.add(angka2);
+        frame.add(label3);
+        frame.add(operator);
 
         frame.setSize(400, 300);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
