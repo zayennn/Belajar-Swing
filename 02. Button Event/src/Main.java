66bfafc254df5a7ba6d0bf28767da010e9b4d5e8
zayennn <zaayeennn@gmail.com>
2026-04-02 18:47:06 +0700

    update

--- 02. Button Event/src/Main.java
+++ 02. Button Event/src/Main.java
@@ -9,20 +9,28 @@ public class Main {
         JFrame frame = new JFrame("bikin button");
         JButton button = new JButton("click aku");
 
-        button.setBounds(130, 100, 140, 50);
+        frame.setLayout(null);
+
+        button.setBounds(100, 100, 100, 50);
 
         button.addActionListener(new ActionListener() {
+//            kalau tombol diklik, jalankan sesuatu
             @Override
             public void actionPerformed(ActionEvent e) {
+//                Method yang dijalankan pas tombol diklik
+//                e = info event
                 JOptionPane.showMessageDialog(null, "button di click");
+//                Ini buat munculin popup (JOptionPane)
+//                null = muncul di tengah layar
+//                teks = isi popup
             }
         });
 
         frame.add(button);
+//        Masukin tombol ke dalam window
 
-        frame.setSize(300, 400);
-        frame.setVisible(true);
+        frame.setSize(400, 300);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLayout(null);
+        frame.setVisible(true);
     }
 }
