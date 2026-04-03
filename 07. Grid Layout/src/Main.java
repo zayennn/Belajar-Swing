b16fb1a1c5bd0cd969ce26cda28f38eb8b27c6ec
zayennn <zaayeennn@gmail.com>
2026-04-03 09:34:29 +0700

    update

--- 07. Grid Layout/src/Main.java
+++ 07. Grid Layout/src/Main.java
@@ -3,3 +3,28 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Grid Kalkulator");
+
+        JTextField display = new JTextField();
+        display.setEditable(false);
+
+        frame.setLayout(new BorderLayout());
+        frame.add(display, BorderLayout.NORTH);
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout());
+
+        String[] tombol = {
+                "7", "8", "9", "/",
+                "4", "5", "6", "*",
+                "1", "2", "3", "-",
+                "0", "C", "=", "+",
+        };
+
+        for (String text : tombol) {
+            JButton button = new JButton(text);
+        }
+    }
+}
