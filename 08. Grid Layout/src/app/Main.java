3f4f3d216fb28a95e95462aab7833549d80f9418
zayennn <zaayeennn@gmail.com>
2026-04-08 14:25:16 +0700

    update

--- 08. Grid Layout/src/app/Main.java
+++ 08. Grid Layout/src/app/Main.java
@@ -1,7 +1,17 @@
 package app;
 
+import module.Mahasiswa;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        ArrayList<Mahasiswa> mahasiswas = new ArrayList<Mahasiswa>();
+
+        System.out.print("Masukan jumlah mahasiswa yang ingin anda input : ");
+        int jumlah = input.nextInt();
 
     }
 }
--- 08. Grid Layout/src/module/Mahasiswa.java
+++ 08. Grid Layout/src/module/Mahasiswa.java
@@ -1,4 +1,13 @@
 package module;
 
 public class Mahasiswa {
+    private String nama;
+    private int umur;
+    private String jurusan;
+
+    public Mahasiswa(String nama, int umur, String jurusan) {
+        this.nama = nama;
+        this.umur = umur;
+        this.jurusan = jurusan;
+    }
 }
